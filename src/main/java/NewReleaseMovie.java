29e078086db7682b9354934b99c949bfc9d4ca8c
Ali Hussein <ali.hussein201480@yahoo.com>
2017-02-22 14:07:06 -0500

    wednesday afternoon

--- src/main/java/NewReleaseMovie.java
+++ src/main/java/NewReleaseMovie.java
@@ -1,18 +1,21 @@
 import java.math.BigDecimal;
+
 public class NewReleaseMovie extends Movie {
-    public NewReleaseMovie(String title) {
-        super(title, "3.00");
-    }
-    @Override
-    public String getPriceCode() {
-        return "NewRelease";
-    }
-    // movie's late fee method 
-    @Override
-    public BigDecimal calculateFees(int days) {
-        BigDecimal amount = new BigDecimal("3.00");
-        BigDecimal rentalDays = new BigDecimal(days);
-        amount = new BigDecimal("3.00").multiply(rentalDays);
-        return amount;
-    }
+	public NewReleaseMovie(String title) {
+		super(title, "3.00");
+	}
+
+	@Override
+	public String getPriceCode() {
+		return "NewRelease";
+	}
+
+	// movie's late fee method
+	@Override
+	public BigDecimal calculateFees(int days) {
+		BigDecimal amount = new BigDecimal("3.00");
+		BigDecimal rentalDays = new BigDecimal(days);
+		amount = new BigDecimal("3.00").multiply(rentalDays);
+		return amount;
+	}
 }
