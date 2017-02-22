ce04c7c90bde100f64fef4150364851b6a4fff01
Ali Hussein <ali.hussein201480@yahoo.com>
2017-02-22 14:08:56 -0500

    wednesday afternoon

--- src/main/java/Movie.java
+++ src/main/java/Movie.java
@@ -1,15 +1,20 @@
 import java.math.BigDecimal;
+
 // abstract method
 public abstract class Movie {
-    String title;
-    String priceCode;
-    public Movie(String title, String priceCode) {
-        this.title = title;
-        this.priceCode = priceCode;
-    }
-    public String getTitle() {
-        return title;
-    }
-    public abstract String getPriceCode();
-    public abstract BigDecimal calculateFees(int days);
+	String title;
+	String priceCode;
+
+	public Movie(String title, String priceCode) {
+		this.title = title;
+		this.priceCode = priceCode;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public abstract String getPriceCode();
+
+	public abstract BigDecimal calculateFees(int days);
 }
