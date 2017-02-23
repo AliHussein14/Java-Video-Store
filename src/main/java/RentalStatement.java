e09c6d434f2ce801838af45fd77e53a582bea93f
Ali Hussein <ali.hussein201480@yahoo.com>
2017-02-22 20:33:50 -0500

    thursday evening

--- src/main/java/RentalStatement.java
+++ src/main/java/RentalStatement.java
@@ -1,7 +1,4 @@
-import java.io.BufferedReader;
 import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.IOException;
 import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -24,7 +21,7 @@ public class RentalStatement {
 
 	public void printToText() throws Exception {
 		// users updated lists of movies/price code and late fees
-		FileOutputStream fileOutput = new FileOutputStream("VideoRecords.txt");
+		FileOutputStream fileOutput = new FileOutputStream("YourCollectionsApp.md");
 		PrintStream printOutput = new PrintStream(fileOutput);
 
 		for (Rental rental : rentals) {
@@ -37,22 +34,5 @@ public class RentalStatement {
 		fileOutput.close();
 		printOutput.close();
 
-	}
-
-	// this will read and print text from the file YourCollectionsApp.md
-	public void readFromText() throws IOException {
-		FileReader fileIn = new FileReader("YourCollectionsApp.md");
-		BufferedReader reader = new BufferedReader(fileIn);
-
-		System.out.println("\n\nThis is a Collection of Rented Movies we have on your CollectionsApp\n");
-
-		String line;
-		while ((line = reader.readLine()) != null) {
-			System.out.println(line);
-		}
-
-		System.out.println("***************************************\n\n\n");
-
-		reader.close();
 	}
 }
